df7ec7d18fae843153b1ac95b742c09848a7d920
jofuentesl <jofuentesle@uoc.edu>
2021-01-09 17:25:59 +0100

    auth

--- src/app/views/auth/auth.component.ts
+++ src/app/views/auth/auth.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '@services/auth.service'
+import { NgForm } from '@angular/forms';
+
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
+  exportAs: 'authForm'
 })
 export class AuthComponent implements OnInit {
 
-  constructor( public AuthService: AuthService) { }
+  constructor( public authService: AuthService ) { }
+
+  onSubmit(form: NgForm) {
+    const email = form.value.email;
+    const password = form.value.password;
+    this.authService.onRegister(email, password);
+    form.reset();
+  }
 
   ngOnInit(): void {
   }
